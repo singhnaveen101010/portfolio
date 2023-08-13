2803b5a25ba6ae2ba2d1d551a75603116e46b3d9
singhnaveen101010 <97439847+singhnaveen101010@users.noreply.github.com>
2023-08-13 12:26:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$("a").on('click', function(event) {
+
+   
+    if (this.hash !== "") {
+     
+      event.preventDefault();
+     var hash = this.hash;
+     $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1500);
+    } 
+  });
